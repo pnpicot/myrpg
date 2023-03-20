cafa7786b04f4ccaff1ee4be1aca14a2744def04
= <=>
2023-03-20 21:33:00 +0100

    test

--- element_utils.c
+++ element_utils.c
@@ -26,23 +26,35 @@ int get_element_layer(s_appdata *adata, void *ref, int type)
 {
     switch (type) {
         case TYPE_RECT:
+            {
             s_rect *rect = (s_rect *) ref;
             return (rect->layer);
+            }
         case TYPE_CIRCLE:
+            {
             s_circle *circle = (s_circle *) ref;
             return (circle->layer);
+            }
         case TYPE_SPRITE:
+            {
             s_sprite *sprite = (s_sprite *) ref;
             return (sprite->layer);
+            }
         case TYPE_TEXT:
+            {
             s_text *text = (s_text *) ref;
             return (text->layer);
+            }
         case TYPE_VERTEX:
+            {
             s_vertex *vertex = (s_vertex *) ref;
             return (vertex->layer);
+            }
         case TYPE_BUTTON:
+            {
             s_button *button = (s_button *) ref;
             return (button->text->layer);
+            }
     }
 }
 
@@ -50,29 +62,41 @@ void delete_element(s_appdata *adata, void *ref, int type)
 {
     switch (type) {
         case TYPE_RECT:
+            {
             s_rect *rect = (s_rect *) ref;
             delete_rect(adata, rect->id);
             break;
+            }
         case TYPE_CIRCLE:
+            {
             s_circle *circle = (s_circle *) ref;
             delete_circle(adata, circle->id);
             break;
+            }
         case TYPE_SPRITE:
+            {
             s_sprite *sprite = (s_sprite *) ref;
             delete_sprite(adata, sprite->id);
             break;
+            }
         case TYPE_TEXT:
+            {
             s_text *text = (s_text *) ref;
             delete_text(adata, text->id);
             break;
+            }
         case TYPE_VERTEX:
+            {
             s_vertex *vertex = (s_vertex *) ref;
             delete_vertex(adata, vertex->id);
             break;
+            }
         case TYPE_BUTTON:
+            {
             s_button *button = (s_button *) ref;
             delete_button(adata, button->id);
             break;
+            }
     }
 }
 
