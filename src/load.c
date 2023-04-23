43ee482ed7f064dd1d3b7695183aff7cd9239bd2
RANOC3004 <aubin.bosquet@epitech.eu>
2023-04-23 17:13:41 +0200

    load next

--- src/load.c
+++ src/load.c
@@ -21,9 +21,74 @@ char **lines, int i)
     }
 }
 
+void modify_game_quests(s_appdata *adata, linked_node *quests, \
+char **lines, int i)
+{
+    char **quest_stat = str_split(lines[i], '~');
+    while (quests != NULL && quests->data != NULL) {
+        s_quest *quest_cur = (s_quest *) quests->data;
+        if (!my_strcmp(quest_cur->title, quest_stat[0])) {
+            quest_cur->completed = my_getnbr(quest_stat[1]);
+            break;
+        }
+        quests = quests->next;
+    }
+}
+
+void modify_game_skill_trees(s_appdata *adata, linked_node *trees, \
+char **lines, int i)
+{
+    char **trees_stat = str_split(lines[i], '~');
+    while (trees != NULL && trees->data != NULL) {
+        s_skill_tree *trees_cur = (s_skill_tree *) trees->data;
+        if (!my_strcmp(trees_cur->id, trees_stat[0])) {
+            trees_cur->y_end = my_getnbr(trees_stat[2]);
+            break;
+        }
+        trees = trees->next;
+    }
+}
+
+void modify_game_entities(s_appdata *adata, linked_node *models, \
+char **lines, int i)
+{
+    char **model_stat = str_split(lines[i], '~');
+    
+    while (models != NULL && models->data != NULL) {
+        s_entity *model = (s_entity *) models->data;
+        s_faction *faction = model->faction;
+        if (!my_strcmp(faction->id, model_stat[0])) {
+            sfClock_restart(model->clock);
+            s_entity *new_entity = copy_entity_model(adata, model);
+            sfVector2f pos = {str_to_float(model_stat[1]), str_to_float(model_stat[2])};
+            new_entity->pos = pos;
+            faction->entity_count++;
+
+            if (new_entity->emiter != NULL) {
+                int win_w = get_int(adata, "win_w");
+                int win_h = get_int(adata, "win_h");
+                (*new_entity->emiter)(adata, new_entity);
+                char *emiter_id = str_add(new_entity->id, "@[:emiter]");
+                pos.x -= adata->game_data->view_pos.x;
+                pos.y -= adata->game_data->view_pos.y;
+                translate_emiter(adata, emiter_id, pos);
+            }
+
+            linked_add(adata->game_data->entities, new_entity);
+        }
+        models = models->next;
+    }
+}
+
 void modify_game(s_appdata *adata, char *content)
 {
     char **lines = str_split(content, '#');
+    int j = 0;
+    while (lines[j] != NULL) {
+        printf("%s\n",lines[j]);
+        j++;
+    }
+
     s_player *player = adata->player;
     player->attack = str_to_float(lines[0]);
     player->defense = str_to_float(lines[1]);
@@ -39,6 +104,46 @@ void modify_game(s_appdata *adata, char *content)
     while (my_strcmp(lines[i], "$array")) {
         linked_node *syringe = adata->game_data->syringes;
         modify_game_syringe(adata, syringe, lines, i);
+        i++;
+    }
+    i += 2;
+
+    while (my_strcmp(lines[i], "$array")) {
+        linked_node *quests = adata->game_data->quests;
+        modify_game_quests(adata, quests, lines, i);
+        i++;
+    }
+    i++;
+
+    adata->game_data->view_pos.x = str_to_float(lines[i]);
+    adata->game_data->view_pos.y = str_to_float(lines[i + 1]);
+    adata->game_data->velocity = str_to_float(lines[i + 2]);
+    adata->game_data->speed_max = str_to_float(lines[i + 3]);
+    adata->game_data->speed.x = str_to_float(lines[i + 4]);
+    adata->game_data->speed.y = str_to_float(lines[i + 5]);
+    i += 7;
+
+    while (my_strcmp(lines[i], "$array")) {
+        linked_node *trees = adata->game_data->skill_trees;
+        modify_game_skill_trees(adata, trees, lines, i);
+        i++;
+    }
+    i += 2;
+
+    free_ll_and_data(&adata->game_data->entities);
+    adata->game_data->entities = linked_new();
+
+
+    linked_node *factions = adata->game_data->factions;
+    while (factions != NULL && factions->data != NULL) {
+        s_faction *faction = (s_faction *) factions->data;
+        faction->entity_count = 0;
+        factions = factions->next;
+    }
+
+    while (my_strcmp(lines[i], "$array")) {
+        linked_node *models = adata->game_data->entity_models;
+        modify_game_entities(adata, models, lines, i);
         i++;
     }
 }
