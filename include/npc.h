bcf773d28b79c93cb8dbf1f24e99dfe6eecd5ad8
= <=>
2023-04-23 22:25:53 +0200

    Fixed header

--- include/npc.h
+++ include/npc.h
@@ -11,3 +11,9 @@
 
 void init_npc(s_appdata *adata);
 void update_npc(s_appdata *adata);
+void update_npc_bubble(s_appdata *adata, char *text);
+void update_npc(s_appdata *adata);
+void init_npc_rect(s_appdata *adata, char *text_id, \
+char *rtex, char *container);
+void init_npc_text_next(s_appdata *adata, char *id);
+void init_npc_text(s_appdata *adata, sfVector2f npc_pos);
